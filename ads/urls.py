4e2b4781c7239d9aef19f312af81dc4af6b4a5a7
dhaanesh-stack <dhaanesh@testpress.in>
2025-10-23 10:17:16 +0530

    Refactor: Separate test cases for each view

--- ads/urls.py
+++ ads/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
-from .views import (
-    AdListView,
-    PostAdView,
-    MyAdsView,
-    AdUpdateView,
-    AdDeleteView,
-)
+from .views import MyAdsView
 
 urlpatterns = [
-    path('', AdListView.as_view(), name='ad_list'),          
-    path('post/', PostAdView.as_view(), name='post_ad'),     
-    path('my-ads/', MyAdsView.as_view(), name='my_ads'),     
-    path('<int:pk>/edit/', AdUpdateView.as_view(), name='edit_ad'),  
-    path('<int:pk>/delete/', AdDeleteView.as_view(), name='delete_ad'),  
+    path("my-ads/", MyAdsView.as_view(), name="my_ads"),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,32 +1,6 @@
-from django.shortcuts import redirect
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 from .models import Ad
-from .forms import AdForm
-
-
-class AdListView(ListView):
-    model = Ad
-    template_name = 'ads/home.html'  
-    context_object_name = 'ads'
-    ordering = ['-created_at']
-    paginate_by = 6
-
-
-class PostAdView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
-    model = Ad
-    form_class = AdForm
-    template_name = 'ads/post_ad.html'
-    success_url = reverse_lazy('my_ads')
-    success_message = "Your ad has been posted successfully!"
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
-
 
 class MyAdsView(LoginRequiredMixin, ListView):
     model = Ad
@@ -35,25 +9,4 @@ class MyAdsView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Ad.objects.filter(user=self.request.user).order_by('-created_at')
-
-
-class AdUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
-    model = Ad
-    form_class = AdForm
-    template_name = 'ads/edit_ad.html'
-    success_url = reverse_lazy('my_ads')
-    success_message = "Your ad has been updated successfully!"
-    def test_func(self):
-        ad = self.get_object()
-        return self.request.user == ad.user
-
-
-class AdDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Ad
-    template_name = 'ads/confirm_delete.html'
-    success_url = reverse_lazy('my_ads')
-
-    def test_func(self):
-        ad = self.get_object()
-        return self.request.user == ad.user
 

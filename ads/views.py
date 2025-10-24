95341e695df42475410d3607f3bf17f9247fd52c
dhaanesh-stack <dhaanesh@testpress.in>
2025-10-24 10:36:30 +0530

    feat: Add EditAdView to allow users to edit their own posted ads

--- ads/views.py
+++ ads/views.py
@@ -1,26 +1,41 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, CreateView, UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from .models import Ad
 from .forms import AdForm
 
+
 class MyAdsView(LoginRequiredMixin, ListView):
     model = Ad
-    template_name = 'ads/my_ads.html'
-    context_object_name = 'ads'
+    template_name = "ads/my_ads.html"
+    context_object_name = "ads"
 
     def get_queryset(self):
-        return Ad.objects.filter(user=self.request.user).order_by('-created_at')
+        return Ad.objects.filter(user=self.request.user).order_by("-created_at")
+
 
 class PostAdView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Ad
     form_class = AdForm
-    template_name = 'ads/post_ad.html'
-    success_url = reverse_lazy('my_ads')
+    template_name = "ads/post_ad.html"
+    success_url = reverse_lazy("my_ads")
     success_message = "Your ad has been posted successfully!"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
+
+
+class AdUpdateView(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView
+):
+    model = Ad
+    form_class = AdForm
+    template_name = "ads/edit_ad.html"
+    success_url = reverse_lazy("my_ads")
+    success_message = "Your ad has been updated successfully!"
+
+    def test_func(self):
+        ad = self.get_object()
+        return self.request.user == ad.user

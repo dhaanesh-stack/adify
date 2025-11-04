779a22045b70b6f11b442f461189560c581870ce
dhaanesh-stack <dhaanesh@testpress.in>
2025-11-04 19:32:47 +0530

    UI enhancement for post forms

--- ads/forms.py
+++ ads/forms.py
@@ -9,3 +9,11 @@ class AdForm(forms.ModelForm):
             'location', 'image', 'contact_email',
             'contact_phone', 'show_contact', 'event_date'
         ]
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        common_classes = "w-full border border-gray-300 rounded-md p-2 outline-none focus:outline-indigo-500 focus:ring-2 focus:ring-indigo-500"
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, (forms.TextInput, forms.EmailInput, forms.NumberInput, forms.URLInput, forms.Textarea, forms.Select)):
+                existing_classes = field.widget.attrs.get("class", "")
+                field.widget.attrs["class"] = f"{existing_classes} {common_classes}".strip()

3ed663bebc2b85946e8d43d7589037e6668917a8
dhaanesh-stack <dhaanesh@testpress.in>
2025-11-04 17:54:24 +0530

    ui: Add consistent styling and placeholders for all authentication forms

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    PasswordResetForm,
+    SetPasswordForm,
+)
 
 User = get_user_model()
+
+INPUT_CLS = "w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -10,7 +17,66 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ["username", "email"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholder_map = {
+            "username": "Username",
+            "email": "Email address",
+            "phone_number": "Phone number (optional)",
+            "password1": "Password",
+            "password2": "Confirm password",
+        }
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                "class": INPUT_CLS,
+                "placeholder": placeholder_map.get(name, ""),
+                "autocomplete": "off" if name in ("password1", "password2") else "on",
+            })
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(max_length=150)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholder_map = {
+            "username": "Username or email",
+            "password": "Password",
+        }
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                "class": INPUT_CLS,
+                "placeholder": placeholder_map.get(name, ""),
+            })
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs.update({
+            "class": INPUT_CLS,
+            "placeholder": "Enter your registered email address",
+        })
+
+
+class CustomSetPasswordForm(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholder_map = {
+            "new_password1": "New password",
+            "new_password2": "Confirm new password",
+        }
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({
+                "class": INPUT_CLS,
+                "placeholder": placeholder_map.get(name, ""),
+                "autocomplete": "off",
+            })

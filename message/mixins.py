3965ee312ca024bd4b874502ff4086f3027bcb8e
dhaanesh-stack <dhaanesh@testpress.in>
2025-11-04 17:28:54 +0530

    Raise 404

--- message/mixins.py
+++ message/mixins.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseForbidden, Http404
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from ads.models import Ad
 from .models import Message
 
+User = get_user_model()
+
 class AdAccessMixin(LoginRequiredMixin):
     def setup_ad_access(self, request, *args, **kwargs):
-        self.ad = get_object_or_404(Ad, pk=kwargs["pk"])
+        ad_pk = kwargs.get("pk") or kwargs.get("ad_id")
+        self.ad = get_object_or_404(Ad, pk=ad_pk)
         self.buyer_id = kwargs.get("buyer_id")
         return request.user
 
@@ -14,20 +18,20 @@ class SellerAccessMixin(AdAccessMixin):
     def check_seller_access(self, user):
         if user.id == self.ad.user_id:
             if not self.buyer_id:
-                return Http404()
+                raise Http404()
             try:
                 buyer_id_int = int(self.buyer_id)
             except (TypeError, ValueError):
-                return Http404()
+                raise Http404()
             if buyer_id_int == user.id:
-                return Http404()
-            if self.buyer_id and not self.user_model.objects.filter(id=buyer_id_int).exists():
-                return Http404()
+                raise Http404()
+            if not User.objects.filter(id=buyer_id_int).exists():
+                raise Http404()
         return None
 
 class BuyerAccessMixin(AdAccessMixin):
     def check_buyer_access(self, user):
         if user.id != self.ad.user_id:
-            if not (Message.objects.filter(ad=self.ad, sender=user).exists() or not self.buyer_id):
+            if not (Message.objects.filter(ad=self.ad, sender=user).exists() or self.buyer_id):
                 return HttpResponseForbidden()
         return None
